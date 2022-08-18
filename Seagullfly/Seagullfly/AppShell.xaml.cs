88704be4537db88d6915be059896a6a656ba98b9
Mariusz Skuza <marsku8@wp.pl>
2022-08-18 23:23:26 +0200

    Add project files.

---
+++ Seagullfly/Seagullfly/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Seagullfly
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Seagullfly/Seagullfly/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using MvvmHelpers;
+
+namespace Seagullfly.ViewModels
+{
+    public class ViewModelBase : BaseViewModel
+    {
+    }
+}
