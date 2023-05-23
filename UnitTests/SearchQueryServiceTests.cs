f52d37d76ab7e11d11710c7182c6ffee106b895f
Mariusz Skuza <marsku8@wp.pl>
2023-05-23 23:31:02 +0200

    add test

--- UnitTests/SearchQueryServiceTests.cs
+++ UnitTests/SearchQueryServiceTests.cs
@@ -1,5 +1,7 @@
 using Moq;
+using SeagullflyMaui.Enums;
 using SeagullflyMaui.Interfaces;
+using SeagullflyMaui.Model;
 using SeagullflyMaui.Services;
 
 namespace UnitTests;
@@ -14,13 +16,25 @@ public class SearchQueryServiceTests
     public void Setup()
     {
 
-
+        seagullflyDatabaseMock.Setup(x => x.GetAll<SearchQuery>()).ReturnsAsync(new List<SearchQuery>() { new SearchQuery
+        {
+            Passengers = new[]
+            {
+                new Passenger(){ Type = PassengerType.Adult, Quantity = 1},
+                new Passenger(){ Type = PassengerType.Youth, Quantity = 2},
+                new Passenger(){ Type = PassengerType.Children, Quantity = 3},
+                new Passenger(){ Type = PassengerType.Infant, Quantity = 4},
+            }.ToList()
+        } });
         searchQueryService = new SearchQueryService(seagullflyDatabaseMock.Object);
     }
 
     [Test]
-    public void Test1()
+    public void SearchQueryServiceReturnNotEmptyListWhenDatabaseReturnData()
     {
-        searchQueryService.GetSavedQuerries();
+        var queries = searchQueryService.GetSavedQuerries().Result;
+
+        Assert.That(queries, Is.Not.Empty);
+        Assert.That(queries, Has.Count.EqualTo(1));
     }
 }
